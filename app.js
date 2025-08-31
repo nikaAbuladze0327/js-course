802014e0fb2fceec559320c4ef784a28f4ef776e
nika <bibobagdasaryan@gamil.com>
2025-08-31 13:55:21 +0400

    added if statemants

---
+++ app.js
@@ -0,0 +1,22 @@
+// if else statement homework
+let score = 100;
+
+if (score < 50) {
+  console.log("ნაკლებია 50-ზე");
+} else if (score > 20) {
+  console.log("მეტია 20-ზე");
+} else {
+  console.log("შეცდომა");
+}
+
+// if mari homework
+let userName = "მარი";
+
+if (userName === "მარი") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// switch case homework
+let userName2 = "მარი" === "მარი" ? console.log("true") : console.log("false");
